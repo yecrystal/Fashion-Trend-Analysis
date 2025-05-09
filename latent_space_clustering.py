fe5d0f3352fd24e4f9b0d244bdd1cf53a69c9a91
yecrystal <crystalllye@gmail.com>
2025-05-09 13:44:16 -0400

    Latent spaces added

--- latent_space_clustering.py
+++ latent_space_clustering.py
@@ -5,7 +5,7 @@ import numpy as np
 def main():
     NUM_CLUSTERS = 10
 
-    df = pd.read_hdf('data/latent_space.h5')
+    df = pd.read_hdf('data/latent_spaces.h5')
     latent_space = np.stack(df['latent_space'].values)
 
     # Creating a KMeans model with chosen number of clusters
@@ -18,7 +18,7 @@ def main():
     df['cluster'] = kmeans.labels_
 
     # Saving data
-    df.to_hdf('data/latent_space.h5', key='df_items', mode='w')
+    df.to_hdf('data/latent_space_clusered.h5', key='df_items', mode='w')
 
 if __name__ == '__main__': 
     main()
